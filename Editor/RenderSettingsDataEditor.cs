c94aad522dbc5c5c24abe4bb0ce447a51c2cb643
HeaX <heax@moonsama.com>
2023-11-06 15:33:30 +0100

    feat: added custom editor

---
+++ Editor/RenderSettingsDataEditor.cs
@@ -0,0 +1,24 @@
+﻿using Essentials;
+using UnityEditor;
+using UnityEngine;
+
+namespace Essentials.Editor
+{
+    [CustomEditor(typeof(RenderSettingsData))]
+    public class RenderSettingsDataEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            var target = (RenderSettingsData) this.target;
+            if (GUILayout.Button("Copy from scene"))
+            {
+                target.CopyFromScene();
+            }
+            else if (GUILayout.Button("Apply to scene"))
+            {
+                target.Apply();
+            }
+        }
+    }
+}
